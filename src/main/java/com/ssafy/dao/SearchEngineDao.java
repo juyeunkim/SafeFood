f21f60e7698aea74c5a352e9357027a407f35788
wjg365 <wjg365@naver.com>
2019-11-25 16:23:34 +0900

    delete + Mapper추가

--- src/main/java/com/ssafy/dao/SearchEngineDao.java
+++ src/main/java/com/ssafy/dao/SearchEngineDao.java
@@ -2,8 +2,11 @@ package com.ssafy.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ssafy.vo.SearchEngine;
 
+@Mapper
 public interface SearchEngineDao {
 	public List<SearchEngine> searchAll();
 	public int getSearchNo();
