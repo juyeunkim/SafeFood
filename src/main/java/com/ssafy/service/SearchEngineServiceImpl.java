0a2dd3574020b3aaecf93807dad6c1d2c17674c6
jleun111 <jleun111@naver.com>
2019-11-25 16:24:03 +0900

    asdf

--- src/main/java/com/ssafy/service/SearchEngineServiceImpl.java
+++ src/main/java/com/ssafy/service/SearchEngineServiceImpl.java
@@ -3,10 +3,11 @@ package com.ssafy.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.dao.SearchEngineDao;
 import com.ssafy.vo.SearchEngine;
-
+@Service
 public class SearchEngineServiceImpl implements SearchEngineService {
 	@Autowired
 	SearchEngineDao sao;
